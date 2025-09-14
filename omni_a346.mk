8bef4c812934ae000762a55f02c00b2075f79b9a
ChrolloUTA <abdolaila00000@gmail.com>
2025-09-14 06:14:52 +0300

    Create omni_a346.mk

---
+++ omni_a346.mk
@@ -0,0 +1,14 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a346
+PRODUCT_NAME := omni_a346
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A346E
+PRODUCT_MANUFACTURER := samsung
