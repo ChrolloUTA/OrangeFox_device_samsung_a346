0e188802871fd4ee096db3add9ed0f879050df80
ChrolloUTA <abdolaila00000@gmail.com>
2025-09-14 06:54:03 +0300

    Update omni_a346.mk

--- omni_a346.mk
+++ omni_a346.mk
@@ -1,12 +1,7 @@
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Inherit from the AOSP product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
-# Device identifier. This must come after all inclusions
+# Device identifier.
 PRODUCT_DEVICE := a346
 PRODUCT_NAME := omni_a346
 PRODUCT_BRAND := samsung
